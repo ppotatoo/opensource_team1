ce6b893459aec507de6da5040e945a9a3e92573b
seokhyeon <gj1515@naver.com>
2023-04-29 19:27:23 +0900

    app.py 수정

--- app.py
+++ app.py
@@ -4,43 +4,28 @@ import os
 import openai
 
 import streamlit as st
-from revChatGPT.V3 import Chatbot
 from streamlit.commands.page_config import RANDOM_EMOJIS
-
-st.set_page_config(page_title="Raynor ChatGPT", page_icon=random.choice(RANDOM_EMOJIS), menu_items={})
-st.title("Demo ChatGPT with Streamlit")
+st.set_page_config(page_title="ChatGPT", page_icon=random.choice(RANDOM_EMOJIS), menu_items={})
+st.title("ChatGPT with Dall-E")
 st.sidebar.header("Chatting")
 st.sidebar.info("Chat")
-
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
-openai.api_key = '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'
+element = st.empty()
 
-def request_to_rev_openai(query):
-    chatbot = Chatbot(api_key="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")
-    prev_text = ""
-    for data in chatbot.ask_stream(prompt=query):
-        if isinstance(data, dict):
-            prev_text = data["message"]
-    if not prev_text:
-        prev_text = "No answer from Raynor ChatGPT"
-    return prev_text
+import chatgpt
+import dalle
 
-def getImage(text):
-    response = openai.Image.create(
-        prompt=text,
-        n=1,
-        size="256x256"
-    )
-    return response["data"][0]["url"]
+openai.api_key = 'api_key'
+
 
 def app_main():
     user_query = st.text_input("Enter").strip()
     if user_query is not None and user_query and user_query != "":
         with st.spinner(text="Waiting for response..."):
-            response = request_to_rev_openai(user_query)
+            response = chatgpt.request_to_rev_openai(user_query)
             st.text(response)
-            image_url = getImage(user_query)
+            image_url = dalle.getImage(user_query)
             st.image(image_url, width=256)
 
 if __name__ == "__main__":
