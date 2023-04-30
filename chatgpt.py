5f007c749884f8dd87aba6b56f095389203b58c2
seokhyeon <gj1515@naver.com>
2023-04-30 21:13:29 +0900

    limit 문제 해결

--- chatgpt.py
+++ chatgpt.py
@@ -6,18 +6,18 @@ element = st.empty()
 
 def request_to_rev_openai(query):
 
-    openai.api_key = "api_key"
+    openai.api_key = ""
     prev_text = ""
 
     messages=[]
-    while True:
-        user_content = query
-        messages.append({"role" : "user", "content" : f"{user_content}"})
+    # while True:
+    user_content = query
+    messages.append({"role" : "user", "content" : f"{user_content}"})
 
-        completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
 
-        assistant_content = completion.choices[0].message['content'].strip()
-        messages.append({"role" : "assistant", "content" : f"{assistant_content}"})
+    assistant_content = completion.choices[0].message['content'].strip()
+    messages.append({"role" : "assistant", "content" : f"{assistant_content}"})
 
-        prev_text = assistant_content
-        element.write(prev_text, unsafe_allow_html=True)
+    prev_text = assistant_content
+    element.write(prev_text, unsafe_allow_html=True)
